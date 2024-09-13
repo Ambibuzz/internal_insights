24c9a485018c38bc6370bf3d2d310206e1a6ac3e
Sambhav Saxena <sambhav.saxena@ambibuzz.com>
2024-09-13 23:03:34 +0530

    added vue form for bigquery credentials
    Signed-off-by: Sambhav Saxena <sambhav.saxena@ambibuzz.com>

---
+++ frontend/src/datasource/ConnectBigQueryDialog.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { computed } from 'vue'
+import BigQueryForm from './BigQueryForm.vue'
+
+const props = defineProps({ show: Boolean })
+const emit = defineEmits(['update:show'])
+const show = computed({
+    get: () => props.show,
+    set: (value) => emit('update:show', value),
+})
+</script>
+<template>
+    <Dialog v-model="show" :options="{ title: 'Connect to BigQuery' }">
+        <template #body-content>
+            <BigQueryForm @submit="show = false" />
+        </template>
+    </Dialog>
+</template>
